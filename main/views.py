41e14a7ea1bdb3d0a3c27df078884d8517419c71
Rafael Okas <21800014@aluno.cotemig.com.br>
2022-11-23 13:41:23 -0300

    Até o roteiro 5

---
+++ main/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    context = {
+        'nome': "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+        'preco': 109.95,
+        'descricao': "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+        "categoria": "men's clothing",
+        "imagem": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+    }
+    return render(request, 'main/index.html', context)
+
+
+def contato (request):
+    return render(request, 'main/contato.html')
+
+def sobre (request):
+    return render(request, 'main/sobre.html')
